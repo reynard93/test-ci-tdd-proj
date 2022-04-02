2e6607ad173d39f66971200d00b90474c7d82bd9
Reylee <reynard.lee@thoughtworks.com>
2022-04-02 15:23:22 +0800

    multiply and divide

--- js/test_money.js
+++ js/test_money.js
@@ -1,14 +1,27 @@
 const assert = require("assert");
 
-class Dollar {
-  constructor(amount) {
+class Money {
+  constructor(amount, currency) {
     this.amount = amount;
+    this.currency = currency;
   }
+
   times(multiplier) {
-    return new Dollar(this.amount * multiplier);
+    return new Money(this.amount * multiplier, this.currency);
+  }
+
+  divide(divisor) {
+    return new Money(this.amount / divisor, this.currency);
   }
 }
 
-let fiver = new Dollar(5);
-let tenner = fiver.times(2);
-assert.strictEqual(tenner.amount, 10);
+let fiver = new Money(5);
+assert.deepStrictEqual(fiver.times(2), new Money(10));
+
+let tenEuros = new Money(10, "EUR");
+assert.strictEqual(tenEuros.times(2).amount, 20);
+assert.deepStrictEqual(tenEuros.times(2).currency, "EUR");
+
+let originalMoney = new Money(4002, "KRW");
+let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
+assert.deepStrictEqual(originalMoney.divide(4), expectedMoneyAfterDivision);
